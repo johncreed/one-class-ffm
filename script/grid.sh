23a1f27adcc69c941c2905e81db249b8fc86995c
johncreed <hsiajuiyang5174@gmail.com>
2018-10-15 23:27:30 +0800

    Update grid.sh

--- script/grid.sh
+++ script/grid.sh
@@ -1,12 +1,37 @@
-data_tr=ob.tr.mf.ffm
-data_te=ob.te.sub.mf.ffm
-item=item.mf.ffm
-k=128
-for w in 0.00390625 0.001953125 0.0009765625 0.00048828125
-do
-    for l in 4
-    do
-        #./train -k $k -l $l -t 41 -r -1 -w $w -c 12 -p ${data_te} ${item} ${data_tr} > logs/${data_tr}.$l.$w
-        ./train -k $k -l $l -t 46 -r -1 -w $w --ns -c 12 -p ${data_te} ${item} ${data_tr} > logs/${data_tr}.$l.$w.ns
-    done
-done
+#!/bin/bash
+
+case $1 in
+  0)
+    # Data
+    tr=ob.tr.mf.ffm
+    te=ob.te.sub.mf.ffm
+    item=item.mf.ffm
+    # Var
+    k=128
+    # extension & logs_pth
+    ext=mf-mf-ns
+    logs_pth=logs/${ext}
+    ;;
+  *)
+    echo "No match"
+    exit
+esac
+
+# Create logs_pth
+mkdir -p logs_pth
+
+# w in 0.00390625 0.001953125 0.0009765625 0.00048828125
+# l in 0.25 1 4 16
+task(){
+  for w in 0.00390625 0.001953125 0.0009765625 0.00048828125
+  do
+      for l in 4
+      do
+        echo "./train -k $k -l $l -t 46 -r -1 -w $w --ns -c 12 -p ${te} ${item} ${tr} > $logs_pth/${tr}.$l.$w.${ext}"
+      done
+  done
+}
+
+# Number of parameter set do in once.
+num_core=1
+task | xargs -d '\n' -P $num_core -I {} sh -c {} &
