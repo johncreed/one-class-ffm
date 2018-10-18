451066abc319f47a8d37bc3ffc74e7a238849dc3
johncreed <b02701216@ntu.edu.tw>
2018-10-18 15:57:31 +0800

    Update kdd12 single field tools

--- script/kdd12.tools/split.py
+++ script/kdd12.tools/split.py
@@ -1,12 +1,24 @@
 #! /usr/bin/python3
 import random as rd
+import sys
 
-tr = open("user.tr.ffm","w")
-te = open("user.te.ffm","w")
-va = open("user.va.ffm","w")
+if len(sys.argv) != 2:
+    print("./split file")
+    sys.exit()
+
+prc_filename = lambda x : (x[:x.rfind(".")] ,x[x.rfind(".")+1:])
+name_tpl = prc_filename(sys.argv[1])
+
+tr = open("{}.tr.{}".format(name_tpl[0], name_tpl[1]),"w")
+te = open("{}.te.{}".format(name_tpl[0], name_tpl[1]),"w")
+va = open("{}.va.{}".format(name_tpl[0], name_tpl[1]),"w")
 
 if __name__ == "__main__":
-    f = open("user.ffm", "r")
+    try:
+        f = open(sys.argv[1], "r")
+    except IOError:
+        print("File not found.")
+        sys.exit()
     rd.seed(0)
     num = 0
     for line in f:
---
+++ script/kdd12.tools/user_fm.py
@@ -0,0 +1,47 @@
+#! /usr/bin/python3
+import csv
+
+# Field 1, 2
+field0 = ['UserID']
+field1 = ['QueryID', 'Depth']
+s_field = field0 + field1
+all_fields = [s_field]
+
+feat_dict = {}
+all_idx = [-1]
+
+
+def add_feat(key, value, field):
+    global all_idx
+    global feat_dict
+    real_key = "{0}:{1}".format(key, value)
+    if real_key in feat_dict:
+        return feat_dict[real_key]
+    all_idx[field] += 1
+    feat_dict[real_key] = all_idx[field]
+    return all_idx[field]
+
+def make_tuple(feat_list,field):
+    feat_str = ["%d:1" % i for i in feat_list]
+    fnc = lambda x: "{}:{}".format(int(field), x)
+    return list(map(fnc, feat_str))
+
+of = open('user.fm', 'w')
+rf = open('user.gby.csv')
+for line in csv.DictReader(rf, delimiter=','):
+    # Lable
+    output = line['AdID'].strip().replace("|", ",")
+    #Feature
+    for i, field_i in enumerate(all_fields):
+        feat_idx_list = []
+        for key in field_i:
+            if line[key] == "":
+                continue
+            values = line[key].split("|")
+            for val in values:
+                feat_idx_list.append(add_feat(key, val.strip(), i))
+        output = "{} {}".format(output," ".join(make_tuple(feat_idx_list, i)))
+
+    print( output, file=of )
+
+print(all_idx)
