9495f350630571ae825c22d351a897c204efab2a
johncreed <hsiajuiyang5174@gmail.com>
2018-10-18 14:51:22 +0800

    Update grid.sh

--- script/grid.sh
+++ script/grid.sh
@@ -12,6 +12,7 @@ case $1 in
     # Var
     k=128
     ns='--ns'
+    t=46
     ;;
   1)
     # Ext & logs_pth
@@ -24,6 +25,7 @@ case $1 in
     # Var
     k=128
     ns='--ns'
+    t=46
     ;;
   2)
     # Ext & logs_pth
@@ -36,6 +38,7 @@ case $1 in
     # Var
     k=128
     ns=''
+    t=46
     ;;
   3)
     # Ext & logs_pth
@@ -46,8 +49,9 @@ case $1 in
     te=user.va.ffm
     item=ad.ffm
     # Var
-    k=64
+    k=16
     ns=''
+    t=200
     logs_pth=logs/${name}.${k}/${ext}
     ;;
   *)
@@ -62,15 +66,15 @@ mkdir -p $logs_pth
 # w in 0.00390625 0.001953125 0.0009765625 0.00048828125
 # l in 0.25 1 4 16
 task(){
-  for w in 0.00390625 0.001953125 0.0009765625 0.00048828125
+  for w in 0.00390625 0.001953125
   do
-      for l in 1
+      for l in 4
       do
-        echo "./train -k $k -l $l -t 46 -r -1 -w $w $ns -c 12 -p ${te} ${item} ${tr} > $logs_pth/${tr}.$l.$w.${ext}"
+        echo "./train -k $k -l $l -t ${t} -r -1 -w $w $ns -c 12 -p ${te} ${item} ${tr} > $logs_pth/${tr}.$l.$w.${ext}"
       done
   done
 }
 
 # Number of parameter set do in once.
-num_core=1
+num_core=2
 task | xargs -d '\n' -P $num_core -I {} sh -c {} &
