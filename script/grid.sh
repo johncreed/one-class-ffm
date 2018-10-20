6ab4e2e4bcc67d04c705cae6de9f3deecb54bc5e
johncreed <hsiajuiyang5174@gmail.com>
2018-10-20 23:59:06 +0800

    Update script

--- script/grid.sh
+++ script/grid.sh
@@ -1,94 +1,118 @@
 #!/bin/bash
 
-case $1 in
-  0)
-    # Ext & logs_pth
-    ext=ffm-ffm
-    # Data
-    name=ob
-    tr=ob.tr.ffm
-    te=ob.va.sub.ffm
-    item=item.ffm
-    # Var
-    k=128
-    ns=''
-    t=70
-    ;;
-  1)
-    # Ext & logs_pth
-    ext=fm-fm
-    # Data
-    name=ob
-    tr=ob.tr.fm
-    te=ob.va.sub.fm
-    item=item.fm
-    # Var
-    k=128
-    ns=''
-    t=70
-    ;;
-  2)
-    # Ext & logs_pth
-    ext=mf-mf
-    # Data
-    name=ob
-    tr=ob.tr.mf
-    te=ob.va.sub.mf
-    item=item.mf
-    # Var
-    k=128
-    ns=''
-    t=70
-    ;;
-  3)
-    # Ext & logs_pth
-    ext=ffm-ffm
-    # Data
-    name=kdd12
-    tr=user.tr.ffm
-    te=user.va.ffm
-    item=ad.ffm
-    # Var
-    k=32
-    ns=''
-    t=200
-    ;;
-  4)
-    # Ext & logs_pth
-    ext=fm-fm
-    # Data
-    name=kdd12
-    tr=user.tr.fm
-    te=user.va.fm
-    item=ad.fm
-    # Var
-    k=64
-    ns=''
-    t=200
-    ;;
-  5)
-    # Ext & logs_pth
-    ext=mf-mf
-    # Data
-    name=kdd12
-    tr=user.tr.mf
-    te=user.va.mf
-    item=ad.mf
-    # Var
-    k=128
-    ns=''
-    t=200
-    ;;
-  *)
-    echo "No match"
-    exit
-esac
-logs_pth=logs/${name}.${k}/${ext}${ns}
+list_solve_type(){
+echo "=== Choose solve type ===
+0: ob, ffm-ffm
+1: ob, fm-fm
+2: ob, mf-mf-ns
+3: kdd, ffm-ffm
+4: kdd, fm-fm
+5: kdd, mf-mf--ns"
+echo -n "sovle (0~5): "
+read solve_type
+}
 
-# 2^0 ~ 2^-11
-w_all=(1 0.5 0.25 0.125 0.0625 0.03125 0.015625 0.0078125 0.00390625 0.001953125 0.0009765625 0.00048828125)
+
+set_up_solve_type(){
+  echo -n "latent vector size(k): "
+  read k
+
+  case $solve_type in
+    0)
+      # Ext & logs_pth
+      tr_ext=ffm
+      i_ext=ffm
+      # Data
+      name=ob
+      tr=ob.tr.${tr_ext}
+      te=ob.va.sub.${tr_ext}
+      item=item.${i_ext}
+      # Var
+      ns=''
+      t=70
+      ext=${tr_ext}-${i_ext}${ns}
+      ;;
+    1)
+      # Ext & logs_pth
+      tr_ext=fm
+      i_ext=fm
+      # Data
+      name=ob
+      tr=ob.tr.${tr_ext}
+      te=ob.va.sub.${tr_ext}
+      item=item.${i_ext}
+      # Var
+      ns=''
+      t=70
+      ext=${tr_ext}-${i_ext}${ns}
+      ;;
+    2)
+      # Ext & logs_pth
+      tr_ext=mf
+      i_ext=mf
+      # Data
+      name=ob
+      tr=ob.tr.${tr_ext}
+      te=ob.va.sub.${tr_ext}
+      item=item.${i_ext}
+      # Var
+      ns='--ns'
+      t=70
+      ext=${tr_ext}-${i_ext}${ns}
+      ;;
+    3)
+      # Ext & logs_pth
+      tr_ext=ffm
+      i_ext=ffm
+      # Data
+      name=kdd12
+      tr=user.tr.${tr_ext}
+      te=user.va.${tr_ext}
+      item=ad.${i_ext}
+      # Var
+      ns=''
+      t=200
+      ext=${tr_ext}-${i_ext}${ns}
+      ;;
+    4)
+      # Ext & logs_pth
+      tr_ext=fm
+      i_ext=fm
+      # Data
+      name=kdd12
+      tr=user.tr.${tr_ext}
+      te=user.va.${tr_ext}
+      item=ad.${i_ext}
+      # Var
+      ns=''
+      t=200
+      ext=${tr_ext}-${i_ext}${ns}
+      ;;
+    5)
+      # Ext & logs_pth
+      tr_ext=mf
+      i_ext=mf
+      # Data
+      name=kdd12
+      tr=user.tr.${tr_ext}
+      te=user.va.${tr_ext}
+      item=ad.${i_ext}
+      # Var
+      ns='--ns'
+      t=200
+      ext=${tr_ext}-${i_ext}${ns}
+      ;;
+    *)
+      echo "No match"
+      exit
+  esac
+  logs_pth=logs/${name}.${k}/${ext}
+}
+
 
 choose_w_list(){
+# 2^0 ~ 2^-11
+w_all=(1 0.5 0.25 0.125 0.0625 0.03125 0.015625 0.0078125 0.00390625 0.001953125 0.0009765625 0.00048828125)
   for i in ${!w_all[@]};
   do
     printf "%s: %s(2^-%s)\n" "$i" "${w_all[$i]}" "$i"
@@ -105,8 +129,8 @@ choose_w_list(){
     w_train+=(${w_all[${idx}]})
   done
 
-  echo -n "w choosed: "
-  echo ${w_train[@]}
+  echo -n "w list = [ "
+  echo ${w_train[@]}]
 }
 
 
@@ -121,31 +145,53 @@ task(){
   done
 }
 
+list_param(){
+echo "===Parameter list==="
+echo "data: $name
+tr: $tr
+va: $te
+item: $item
+k: $k
+t: $t
+ns: $ns
+w list: ${w_train[@]}
+log_path: ${logs_pth}"
+}
+
 grid(){
-  
-  # Create logs_pth
-  echo "===Data: $name solve: $ext==="
-  mkdir -p $logs_pth
-  echo "++++++++++++++++++++++++++"
+
+  # Choose solve type
+  clear
+  list_solve_type
+  set_up_solve_type
   
   # Set w range
+  clear
   echo "===Set w range, -1 to exit==="
   choose_w_list
   echo "++++++++++++++++++++++++++"
   
+  # logs_pth
+  
+  # Check the right command
+  clear
+  list_param
+  echo "===All cmd to run==="
+  task
+
   # Number of parameter set do in once.
   echo -n "Number of param run at once: "
   read num_core
   echo "++++++++++++++++++++++++++"
-  
-  # Check the right command
-  echo "===All cmd to run==="
-  task
+
+  # Start or not
   echo -n "Start ? [y/n]"
   read std
   echo "++++++++++++++++++++++++++"
   if [[ $std =~ y ]]
   then
+    # Create logs_pth
+    mkdir -p $logs_pth
     echo "run"
     task | xargs -d '\n' -P $num_core -I {} sh -c {} &
   else
