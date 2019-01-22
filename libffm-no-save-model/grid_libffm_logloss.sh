0736cbc4cde684a5b9c1379696bd65ca0873e30b
johncreed <hsiajuiyang5174@gmail.com>
2019-01-23 02:26:29 +0800

    No save train

---
+++ libffm-no-save-model/grid_libffm_logloss.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+num_core=20
+
+r_train=(0.4 0.1 0.025 0.00625 0.0015625 0.000390625)
+l_train=(1e-6 1e-5 1e-4 1e-3)
+k=32
+t=20
+libffm_tr="ob.tr.libffm"
+libffm_va="ob.tr.libffm"
+logs_pth="logs/ob.32"
+
+mkdir -p $logs_pth
+
+task(){
+  for r in ${r_train[@]} 
+  do
+      for l in ${l_train[@]}
+      do
+        echo "./ffm-train -k $k -l $l -t $t -r $r --no-norm ${libffm_tr} $models_pth/${libffm_tr}.$l.$k.$t.$r/${libffm_tr}.$l.$k.$t.$r > ${logs_pth}/${libffm_tr}.$l.$k.$t.$r"
+      done
+  done
+}
+
+task > .task_file.tmp
+cat .task_file.tmp | xargs -d '\n' -P $num_core -I {} sh -c {} &
+
